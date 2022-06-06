068ba4ff612b7afcf4adb4b6e6802b82b1f172f0
Andrew Mituykov <bobr_degit@bk.ru>
2022-06-06 16:14:35 +0300

    init

---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,13 @@
+package web.service;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+    User getUserById(long id);
+    void saveUser(User user);
+    void deleteUserById(long id);
+    void updateUser(User user);
+}
